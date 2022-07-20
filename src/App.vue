985e42fd903534b04f5c1e5896a7445f6b619e30
duy.ntHachix <duy.nt1009@gmail.com>
2022-07-20 09:55:55 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<img alt="Vue logo" src="./assets/logo.png" />
+	<!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+	<user :age="age" :name="name"></user>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from "./components/HelloWorld.vue";
+import User from "./components/User.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+	components: {
+		User,
+		// HelloWorld,
+	},
+	data() {
+		return {
+			age: 20,
+			name: "Duy",
+		};
+	},
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
---
+++ src/components/User.vue
@@ -0,0 +1,16 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+	<p>The user {{ name }} is {{ age }} year old</p>
+</template>
+
+<script>
+// import { Agent } from "http";
+
+export default {
+	// eslint-disable-next-line vue/multi-word-component-names
+	name: "User",
+	props: ["age", "name"],
+};
+</script>
+
+<style></style>
