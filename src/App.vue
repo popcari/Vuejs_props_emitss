c34ae0d177d8f2b5da5089983d3e4a2e4e8c9699
duythanhnguyen <duy.nt1009@gmail.com>
2022-07-21 00:02:42 +0700

    Revert "init commit"
    This reverts commit 61876f312ff83e54ad967df85de5d9202f1cefab.

--- src/App.vue
+++ src/App.vue
@@ -1,87 +1,53 @@
 <template>
-	<form >
-		<label>To do by Vue3</label>
-		<hr/>
-	</form>
-		<div class="submit-area">
-			<input v-model="newTodo" type="text" placeholder="Add new">
-			<button @click.prevent="addNew">Submit</button>
-		</div>
-		<ul>
-			<li v-for="(todo, index) in todos" :key="todo.id" class="todo">
-			<h3 :class="{done:todo.done}" @click="toggleDone(todo)">{{todo.content}} </h3>
-			<button @click="removeTodo(index)">remove</button>
-		</li>
-		</ul>
+	<img alt="Vue logo" src="./assets/logo.png" />
+	<hr />
+	<!-- <button type="button" @click="age++">update age</button>
 
+	<button type="button" @click="age--">downgrade age</button> -->
+	<user
+		:age="age"
+		:name="name"
+		@up-age="upAgeby2"
+		@down-age="age--"
+		:ageChangeFunc="upAgeby2CB"
+	></user>
+	<greeting :age="age"></greeting>
 </template>
 
 <script>
 // import HelloWorld from "./components/HelloWorld.vue";
-// import { tSModuleDeclaration } from '@babel/types';
-import {ref} from 'vue'
-
+import User from "./components/User.vue";
+import Greeting from "./components/Greeting.vue";
 export default {
 	name: "App",
 	components: {
-	
+		User,
+		Greeting,
 	},
-	setup(){
-		function addNew(){
-			todos.value.push({
-				id: Date.now(),
-				done: false,
-				content: newTodo.value,
-			});
-			newTodo.value = "";
-		}
-		const newTodo = ref("");
-		const todos = ref([]);
-
-		function toggleDone(todo){
-			todo.done = !todo.done;
-		}
-		function removeTodo(index){
-			todos.value.splice(index, 1)
-		}
-		return{
-			addNew,  
-			newTodo,
-			todos,
-			toggleDone,
-			removeTodo
-			}}
-	}
+	data() {
+		return {
+			age: 18,
+			name: "Duy",
+		};
+	},
+	methods: {
+		upAgeby2(num) {
+			this.age += num;
+		},
+		upAgeby2CB(num) {
+			this.age += num;
+		},
+	},
+};
 </script>
 
 <style>
-form{
-	display:flex;
-	align-items:center;
-	justify-content:center;
-	flex-direction:column;
-}
-.submit-area{
-display: flex;
-justify-content: space-around;
-}
-input{
-	padding: 10px 10px;
-	width: 60%;
-}
-
-h3{
-width: 60vw;
-	/* background-color: rgb(238, 203, 157); */
-	color: rgb(0, 0, 0);
-}
-li{
-	list-style-type:decimal;
-}
-.done{
-	text-decoration: line-through;
-}
-.todo{
-	cursor: pointer;
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
---
+++ src/components/User.vue
@@ -0,0 +1,63 @@
+<template>
+	<div class="button">
+		<button type="button" @click="upAge">update age event</button>
+		<button type="button" @click="downAge">downgrade age</button>
+		<!-- test callback function -->
+		<button type="button" @click="ageChangeFunc(3)">
+			Age +3(callback function)
+		</button>
+	</div>
+	<p>The user {{ name }} is {{ age }} year old</p>
+	<p>{{ agex2 }}</p>
+	<p>{{ agex3 }}</p>
+</template>
+
+<script>
+export default {
+	// eslint-disable-next-line vue/multi-word-component-names
+	name: "User",
+	props: {
+		age: {
+			type: Number,
+			require: true,
+		},
+		name: {
+			type: String,
+			require: true,
+		},
+		ageChangeFunc: Function,
+	},
+	emits: ["up-age"],
+	computed: {
+		agex2() {
+			return this.age * 2;
+		},
+		agex3() {
+			return this.age * 3;
+		},
+	},
+	methods: {
+		upAge() {
+			this.$emit("up-age", 2);
+		},
+		downAge() {
+			this.$emit("down-age");
+		},
+	},
+};
+</script>
+
+<style scoped>
+p {
+	color: rgb(8, 172, 62);
+}
+.button {
+	display: flex;
+	justify-content: space-between;
+}
+button {
+	cursor: pointer;
+	color: white;
+	background-image: linear-gradient(to right, #069862, rgba(22, 38, 53, 0.897));
+}
+</style>
